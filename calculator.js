6260016ab5ec51fe5b0c8b9f35b65f1add73aa64
David Pestana <david.pestana.perdomo@gmail.com>
2021-02-09 14:08:50 +0100

    proceso de explicacion

--- calculator.js
+++ calculator.js
@@ -1,23 +1,61 @@
+class State {
+    state = [];
+    onChange = () => {};
+
+    constructor(initial, onChange) {
+        this.set(initial);
+        this.onChange = onChange;
+    }
+
+    set(newState){
+        // newState = {... this.get(), newState};
+        this.state.push(newState);
+        this.onChange(this.get(),this.state.length,this.state);
+    }
+
+    reset() {
+        const initial = this.state[0];
+        this.set(initial);
+    }
+
+    hardReset() {
+        const initial = this.state[0];
+        this.state = [];
+        this.set(initial);
+    }
+
+    get(){
+        return this.state.slice(-1)[0];
+    }
+
+
+}
+
+
 class Calculator  {
     
     domElement;
+    name;
+    buttons;
+    state;
+
 
 
     settings = {
         buttons: [
-            {name: 1, action: () => {}},
-            {name: 2, action: () => {}},
-            // {name: 3, action: (n) => this.setNumberAction(n)},
-            // {name: 4, action: (n) => this.setNumberAction(n)},
-            // {name: 5, action: (n) => this.setNumberAction(n)},
-            // {name: 6, action: (n) => this.setNumberAction(n)},
-            // {name: 7, action: (n) => this.setNumberAction(n)},
-            // {name: 8, action: (n) => this.setNumberAction(n)},
-            // {name: 9, action: (n) => this.setNumberAction(n)},
-            // {name: 0, action: (n) => this.setNumberAction(n)},
-            // {name: '=', action: (o) => this.setOperationAction(o)},
-            // {name: '+', action: (o) => this.setOperationAction(o)},
-            // {name: '-', action: (o) => this.setOperationAction(o)},
+            {name: 1, action: () => {this.setNumberAction(1)}},
+            {name: 2, action: () => {this.setNumberAction(2)}},
+            {name: 3, action: () => this.setNumberAction(3)},
+            {name: 4, action: () => this.setNumberAction(4)},
+            {name: 5, action: () => this.setNumberAction(5)},
+            {name: 6, action: () => this.setNumberAction(6)},
+            {name: 7, action: () => this.setNumberAction(7)},
+            {name: 8, action: () => this.setNumberAction(8)},
+            {name: 9, action: () => this.setNumberAction(9)},
+            {name: 0, action: () => this.setNumberAction(0)},
+            {name: '=', action: () => this.setOperationAction('=')},
+            {name: '+', action: () => this.setOperationAction('+')},
+            {name: '-', action: () => this.setOperationAction('-')},
             // {name: '/', action: (o) => this.setOperationAction(o)},
             // {name: '*', action: (o) => this.setOperationAction(o)},
             // {name: '^', action: (o) => this.setOperationAction(o)},
@@ -30,14 +68,30 @@ class Calculator  {
 
 
     constructor(elementId) {
+        this.name = elementId;
         this.domElement = document.getElementById(elementId);
+        this.domElement.setAttribute('class','calculator');
         this.init();
 
     }
 
     init() {
-        this.buildButtons();
 
+        let initialState ={
+                num1: 0,
+                num2: null,
+                operation: null
+            };
+
+        let stateCallbackOnChange = (a, lenght, allSates) => {
+            console.log(a);
+        
+            console.log('el estado ha cambiado', this.name);
+        }
+
+
+        this.buildButtons();
+        this.state = new State(initialState,stateCallbackOnChange);
     }
 
     render() {
@@ -47,15 +101,61 @@ class Calculator  {
 
     buildButtons(){
 
-        let colleccionDeInstanciasDeLaClaseBoton = this.settings.buttons.map(
+        this.buttons = this.settings.buttons.map(
             (element) => {return new Button(element.name,element.action)}
         )
 
-        colleccionDeInstanciasDeLaClaseBoton.map((button) => button.appendTo(this.domElement));
+        this.buttons.map((button) => button.appendTo(this.domElement));
             
-
     }
 
+
+    // Actions
+    
+
+    setNumberAction(number) {
+
+        let {num1,num2,operation} = this.state.get();
+
+        if(operation == null) {
+            num1 = num1 * 10 + number;
+        } else {
+            num2 = num2 * 10 + number;
+        }
+        this.state.set({num1,num2,operation});
+    }
+
+
+    setOperationAction(newOperation) {
+        let {num1,num2,operation} = this.state.get();
+        if(num2 != null && operation!= null) {
+            num1 = this.calcule();    
+        }
+        if(newOperation == '=') {
+            operation = null;
+            num2 = null;
+        } else {
+            operation = newOperation;
+            num2 = 0;
+        };
+        this.state.set({num1,num2,operation});
+    }
+
+
+    calcule(){
+        let {num1,num2,operation} = this.state.get();
+
+        switch(operation) {
+            case '+': return num1*1 + num2*1;
+            case '-': return num1*1 - num2*1;
+            case '/': return num1*1 / num2*1;
+            case '*': return num1*1 * num2*1;
+            case '^': return Math.pow(num1*1,num2*1);
+
+
+            break;
+        }
+    }
 
 }
 
@@ -102,15 +202,5 @@ class Button {
 
     }
 
-
-}
-
-
-
-class State {
-
-    constructor () {
-
-    }
 
 }
